d24ce0266befa876ef550dbb8b98609536444062
FNetoF <netofelipeferreira@hotmail.com>
2022-10-21 15:15:08 -0300

    Projeto Titanic

--- 5_projeto_titanic/projeto_titanic.py
+++ 5_projeto_titanic/projeto_titanic.py
@@ -11,10 +11,8 @@ df = titanic[['Name', 'Age', 'Sex', 'Pclass', 'Survived']]
 # filtrar  nome por ordem alfabética
 df = df.sort_values(['Name'], ascending = True)
 
-
 # filtrar: somente sexo feminino, classe somente 1º classe e se sobreviveu 
 df = df.loc[(df['Sex'] == 'female') & (df['Pclass'] == 1) & (df['Survived'] == 1)]
-
 df
 
 # salvar o arquivo em csv
